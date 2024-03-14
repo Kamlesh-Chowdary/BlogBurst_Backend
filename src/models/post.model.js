d1ccb20a82f14969171e00a96b1dff64f22c697d
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-03-14 18:57:19 +0530

    add user  and post model

---
+++ src/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      maxlength: 30,
+      unique: true,
+    },
+    featuredImage: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Post = mongoose.model("Post", postSchema);
