f96a27060625d598f364a49484b9f3a218fa5372
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-03-17 19:44:33 +0530

    router integration in app

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//importing routes
+
+import { userRouter } from "./routes/user.router.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
