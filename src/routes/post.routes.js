6eb52bf47fa3780f0ab133a5384f443e9e62e4a4
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-03-22 19:27:42 +0530

    add user authorization to post routes

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,8 +1,13 @@
 import { upload } from "../middlewares/multer.middleware.js";
 import { Router } from "express";
 import { createPost, getPostById } from "../controllers/post.controller.js";
+import { verifyUser } from "../middlewares/auth.middleware.js";
 const postRouter = Router();
 
-postRouter.route("/create-post").post(upload.single("post"), createPost);
+// Apply verifyUser middleware to all routes in this file
+postRouter.use(verifyUser);
+postRouter
+  .route("/create-post")
+  .post(upload.single("featuredImage"), createPost);
 postRouter.route("/:id").get(getPostById);
 export { postRouter };
